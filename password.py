c187c3a85122ffce4dc830cd08d9d4629f9bb4d7
JackChen0 <stu30305@gmail.com>
2018-12-27 21:36:47 +0800

    Second version

--- password.py
+++ password.py
@@ -2,7 +2,7 @@ password_c = 'a123456'
 time = 3
 while True:	
 	password_t = input('Enter your password: ')
-	if password_t =='a123456':
+	if password_t == password_c:
 		print('Successfully log in !!!')
 		break
 	else:

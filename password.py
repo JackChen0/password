f6e5fa1cf9eb48af0a4411a86c42ff1e31ba2ca2
JackChen0 <stu30305@gmail.com>
2018-12-27 21:22:13 +0800

    first commit

---
+++ password.py
@@ -0,0 +1,13 @@
+password_c = 'a123456'
+time = 3
+while True:	
+	password_t = input('Enter your password: ')
+	if password_t =='a123456':
+		print('Successfully log in !!!')
+		break
+	else:
+		time = time - 1
+		print('Please try again,and you have',time,'times.')
+		if time == 0:
+			print('Goodbye !!!')
+			break
